ba835ca67f2ad1e99a7cf159322b7825da8c8dff
WellAndGood <danielpisani12@hotmail.com>
2024-03-07 20:01:36 -0500

    code-along without live environment to follow with tutorial

--- js/math/utils.js
+++ js/math/utils.js
@@ -14,6 +14,10 @@ function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER) {
  function distance(p1, p2) {
     return Math.hypot(p1.x - p2.x, p1.y - p2.y);
  }
+
+ function average(p1, p2) {
+   return new Point((p1.x + p2.x) / 2, (p1.y + p2.x) / 2);
+ }
  
  function add(p1, p2) {
     return new Point(p1.x + p2.x, p1.y + p2.y);
@@ -38,24 +42,24 @@ function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER) {
    return Math.atan2(p.y, p.x);
  }
 
-//  function getIntersection(A, B, C, D) {
-//    const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
-//    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
-//    const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+ function getIntersection(A, B, C, D) {
+   const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+   const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+   const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
 
-//    if (bottom != 0) {
-//       const t = tTop / bottom;
-//       const u = uTop / bottom;
-//       if (t >= 0 && t <=1 && u >= 0 && u <= 1) {
-//          return {
-//             x: lerp(A.x, B.x, t),
-//             y: lerp(A.y, B.y, t),
-//             offset: t
-//          }
-//       }
-//    }
-//    return null
-//  }
+   if (bottom != 0) {
+      const t = tTop / bottom;
+      const u = uTop / bottom;
+      if (t >= 0 && t <=1 && u >= 0 && u <= 1) {
+         return {
+            x: lerp(A.x, B.x, t),
+            y: lerp(A.y, B.y, t),
+            offset: t
+         }
+      }
+   }
+   return null
+ }
 
  function lerp(a, b, t) {
    return a + (b - a) * t;
