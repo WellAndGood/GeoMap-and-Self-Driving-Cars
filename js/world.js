52d2bdaa77ea7f2c915cb622b0dcabbdb309c808
WellAndGood <danielpisani12@hotmail.com>
2024-03-10 00:41:19 -0500

    created and refactored classes for markings, stop lines, crossings. Now class inheritance.

--- js/world.js
+++ js/world.js
@@ -20,6 +20,9 @@ class World {
         this.roadBorders = [];
         this.buildings = [];
         this.trees = [];
+        this.laneGuides = [];
+
+        this.markings = [];
 
         this.generate();
     }
@@ -35,6 +38,24 @@ class World {
         this.roadBorders = Polygon.union(this.envelopes.map((e) => e.poly))
         this.buildings = this.#generateBuildings();
         this.trees = this.#generateTrees();
+        
+        this.laneGuides.length = 0;
+        this.laneGuides.push(...this.#generateLaneGuides());
+    }
+
+    #generateLaneGuides() {
+        const tmpEnvelopes = [];
+        for (const seg of this.graph.segments) {
+            tmpEnvelopes.push(
+                new Envelope(
+                    seg,
+                    this.roadWidth / 2,
+                    this.roadRoundness
+                )
+            );
+        }
+        const segments = Polygon.union(tmpEnvelopes.map((e) => e.poly));
+        return segments
     }
 
     #generateTrees() {
@@ -168,6 +189,11 @@ class World {
         for (const env of this.envelopes) {
             env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
         }
+
+        for (const marking of this.markings) {
+            marking.draw(ctx);
+        }
+        
         for (const seg of this.graph.segments) {
             seg.draw(ctx, { color: "white", width: 4, dash: [10, 10]});
         }
@@ -187,8 +213,11 @@ class World {
         for (const item of items) {
             item.draw(ctx, viewPoint);
         }
-        // for (const bld of this.buildings) {
-        //     bld.draw(ctx, viewPoint);
-        // }
+
+        // Looping through of all of the laneGuides
+        for (const seg of this.laneGuides) {
+            seg.draw(ctx, {color: "red"});
+        }
+
     }
 }
