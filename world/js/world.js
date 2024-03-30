22d24720a151f12f385947e51a3ae75b8bfa182f
WellAndGood <danielpisani12@hotmail.com>
2024-03-29 20:59:27 -0700

    place all self-driving code into world-generator. Cars are now trained over self-created world

--- world/js/world.js
+++ world/js/world.js
@@ -23,6 +23,9 @@ class World {
        this.laneGuides = [];
  
        this.markings = [];
+       
+       this.cars = [];
+       this.bestCar = null;
  
        this.frameCount = 0;
  
@@ -263,14 +266,16 @@ class World {
        this.frameCount++;
     }
  
-    draw(ctx, viewPoint) {
+    draw(ctx, viewPoint, showStartMarkings = true) {
        this.#updateLights();
  
        for (const env of this.envelopes) {
           env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
        }
        for (const marking of this.markings) {
-          marking.draw(ctx);
+         if (!(marking instanceof Start) || showStartMarkings) {
+            marking.draw(ctx);
+         }
        }
        for (const seg of this.graph.segments) {
           seg.draw(ctx, { color: "white", width: 4, dash: [10, 10] });
@@ -279,6 +284,16 @@ class World {
           seg.draw(ctx, { color: "white", width: 4 });
        }
  
+      ctx.globalAlpha = 0.2;
+       for (const car of this.cars) {
+         car.draw(ctx);
+       }
+
+       ctx.globalAlpha = 1;
+       if (this.bestCar) {
+         this.bestCar.draw(ctx, true);
+       }
+
        const items = [...this.buildings, ...this.trees];
        items.sort(
           (a, b) =>
